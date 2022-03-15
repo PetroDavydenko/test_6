3a813ac783cedc804919922c1333c5311255d65d
Admin <petrodavidenko32@gmail.com>
2022-03-15 21:08:43 +0200

    Create and use class Operation

---
+++ src/Operation.java
@@ -0,0 +1,14 @@
+public class Operation {
+
+    public int increment(int number) {
+        return ++number;
+    }
+
+    public int decrement(int number) {
+        return --number;
+    }
+
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+}
--- src/test_6.java
+++ src/test_6.java
@@ -1,7 +1,13 @@
 public class test_6 {
     public static void main(String[] arg) {
-        int n = 5;
-        n++;
-        System.out.print(n);
+        int a = 5;
+        int b = 2;
+        Operation operation = new Operation();
+        int result1 = operation.increment(a);
+        System.out.println("Result of increment = " + result1);
+        int result2 = operation.decrement(a);
+        System.out.println("Result of decrement = " + result2);
+        int result3 = operation.subtraction(a, b);
+        System.out.println("Result of subtraction = " + result3);
     }
 }
